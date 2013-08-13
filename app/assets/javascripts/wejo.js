9b3f0f5df1d4005710efd4f2a54dc8fe777593b6
Luke Persola <lukepersola@gmail.com>
2013-08-12 23:25:06 -0700

    sliding display tinkering

---
+++ app/assets/javascripts/wejo.js
@@ -0,0 +1,13 @@
+Wejo = {
+	start: function() {
+		console.log("BLAH");
+	},
+
+	to1slide: function() {
+		$('.slide2').animate({'left': '0px'});
+	},
+
+	to2slides: function() {
+		$('.slide2').animate({'left': '207px'});
+	}
+};
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wejo::Application.config.secret_token = '95da1296d24f693b46f05ec623d456dfb88c65f9af6fd63ca5a64e26b5250a26c6a0fb76c1177c5532920d329dfd2011cd85ebd640dd9c937d859a5c048b126e'
